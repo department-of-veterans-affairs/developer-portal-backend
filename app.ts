6ec6c4182e88ff7c3a9ad903ad08ca1d317020e1
Ryan Travitz <rtravitz@gmail.com>
2020-03-25 12:44:26 -0400

    Add initial connection to lambda code

--- app.ts
+++ app.ts
@@ -1,16 +1,103 @@
-import express from 'express';
-import { config, DynamoDB } from 'aws-sdk';
+import express from 'express'
+import { config, DynamoDB } from 'aws-sdk'
 
 import {
   GovDeliveryClient,
   KongClient,
   KongConfig,
   OktaClient,
-  SentryClient,
   SlackClient,
-} from './lib';
+} from './lib'
 
 import developerApplicationHandler from './routes/DeveloperApplication'
+
+const configureGovDeliveryClient = (): GovDeliveryClient | null => {
+  const { GOVDELIVERY_KEY, GOVDELIVERY_HOST } = process.env
+  let client
+
+  if (GOVDELIVERY_KEY && GOVDELIVERY_HOST) {
+    client = new GovDeliveryClient({
+      host: GOVDELIVERY_HOST,
+      token: GOVDELIVERY_KEY,
+    })
+  }
+
+  return client
+}
+
+const configureKongClient = (): KongClient => {
+  const { KONG_KEY, KONG_HOST, KONG_PROTOCOL } = process.env
+
+  if (KONG_KEY && KONG_HOST) {
+    const kongfig: KongConfig = {
+      apiKey: KONG_KEY,
+      host: KONG_HOST,
+    }
+    if (KONG_PROTOCOL === 'http' || KONG_PROTOCOL === 'https') {
+      kongfig.protocol = KONG_PROTOCOL
+    }
+    return new KongClient(kongfig)
+  } else {
+    throw new Error('Kong Config Missing')
+  }
+}
+
+const configureOktaClient = (): OktaClient | null => {
+  const { OKTA_TOKEN, OKTA_ORG } = process.env
+  let client
+
+  if (OKTA_TOKEN && OKTA_ORG) {
+    client = new OktaClient({
+      org: OKTA_ORG,
+      token: OKTA_TOKEN,
+    })
+  }
+
+  return client
+}
+
+const configureSlackClient = (): SlackClient | null => {
+  const { SLACK_TOKEN, SLACK_CHANNEL_ID } = process.env
+  let client
+
+  if (SLACK_TOKEN && SLACK_CHANNEL_ID) {
+    client = new SlackClient({
+      channelID: SLACK_CHANNEL_ID,
+      token: SLACK_TOKEN,
+    })
+  }
+
+  return client
+}
+
+interface HttpOptions {
+  timeout: number;
+}
+
+interface DynamoConfig {
+  httpOptions: HttpOptions;
+  maxRetries: number;
+  endpoint?: string | undefined;
+}
+
+const configureDynamoDBClient = (): DynamoDB.DocumentClient => {
+  const dynamoConfig: DynamoConfig = {
+    httpOptions: {
+      timeout: 5000,
+    },
+    maxRetries: 1,
+  }
+  if (process.env.NODE_ENV !== 'production') {
+    config.update({
+      accessKeyId: 'NONE',
+      region: 'us-west-2',
+      secretAccessKey: 'NONE',
+    })
+    dynamoConfig.endpoint = process.env.DYNAMODB_ENDPOINT
+  }
+
+  return new DynamoDB.DocumentClient(dynamoConfig)
+}
 
 export default function configureApp(): express.Application {
   const app = express()
@@ -33,81 +120,3 @@ export default function configureApp(): express.Application {
 
   return app
 }
-
-const configureGovDeliveryClient = (): GovDeliveryClient | null => {
-  const { GOVDELIVERY_KEY, GOVDELIVERY_HOST } = process.env;
-  let client;
-
-  if (GOVDELIVERY_KEY && GOVDELIVERY_HOST) {
-    client = new GovDeliveryClient({
-      host: GOVDELIVERY_HOST,
-      token: GOVDELIVERY_KEY,
-    });
-  }
-
-  return client;
-};
-
-const configureKongClient = (): KongClient => {
-  const { KONG_KEY, KONG_HOST, KONG_PROTOCOL } = process.env;
-
-  if (KONG_KEY && KONG_HOST) {
-    const kongfig: KongConfig = {
-      apiKey: KONG_KEY,
-      host: KONG_HOST,
-    };
-    if (KONG_PROTOCOL === 'http' || KONG_PROTOCOL === 'https') {
-      kongfig.protocol = KONG_PROTOCOL;
-    }
-    return new KongClient(kongfig);
-  } else {
-    throw new Error('Kong Config Missing');
-  }
-};
-
-const configureOktaClient = (): OktaClient | null => {
-  const { OKTA_TOKEN, OKTA_ORG } = process.env;
-  let client;
-
-  if (OKTA_TOKEN && OKTA_ORG) {
-    client = new OktaClient({
-      org: OKTA_ORG,
-      token: OKTA_TOKEN,
-    });
-  }
-
-  return client;
-};
-
-const configureSlackClient = (): SlackClient | null => {
-  const { SLACK_TOKEN, SLACK_CHANNEL_ID } = process.env;
-  let client;
-
-  if (SLACK_TOKEN && SLACK_CHANNEL_ID) {
-    client = new SlackClient({
-      channelID: SLACK_CHANNEL_ID,
-      token: SLACK_TOKEN,
-    });
-  }
-
-  return client;
-};
-
-const configureDynamoDBClient = () => {
-  const dynamoConfig: any = {
-    httpOptions: {
-      timeout: 5000,
-    },
-    maxRetries: 1,
-  };
-  if (process.env.NODE_ENV !== 'production') {
-    config.update({
-      accessKeyId: 'NONE',
-      region: 'us-west-2',
-      secretAccessKey: 'NONE',
-    });
-    dynamoConfig.endpoint = process.env.DYNAMODB_ENDPOINT;
-  }
-
-  return new DynamoDB.DocumentClient(dynamoConfig);
-};
