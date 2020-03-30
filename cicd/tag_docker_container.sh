571fabd07a8a8b86d34d49dedb60f97ae467bd28
Demian Ginther <st.diluted@gmail.com>
2020-03-30 11:38:02 -0600

    try different way

--- cicd/tag_docker_container.sh
+++ cicd/tag_docker_container.sh
@@ -8,26 +8,30 @@ command -v jq >/dev/null 2>&1 || { echo >&2 "I require jq but it's not installed
 
 REPOSITORY="dvp/developer-portal-backend"
 JOBS="$(aws codebuild list-builds-for-project --project-name dev-portal-backend-ci|jq -r .ids[])"
+TEST_COMMAND="aws codebuild batch-get-builds --ids $JOBS | jq -r --arg COMMIT_ID $2 '.builds[] | select(.resolvedSourceVersion | contains($COMMIT_ID)) | .buildStatus'"
+MAX_TRIES=100
 
-while read -r status; do
-  case $status in
-    IN_PROGRESS)
-      echo "Build for $2 still in progress..."
-      sleep 5
-      ;;
-    SUCCEEDED)
-      echo "Found successful build for $2. Tagging."
-      if [[ "$(aws ecr describe-images --repository-name "$REPOSITORY" --image-ids imageTag="$2"|jq -r .imageDetails[].imageTags[])" == "$2" ]]; then
-        MANIFEST="$(aws ecr batch-get-image --repository-name "$REPOSITORY" --image-ids imageTag="$2" --query 'images[].imageManifest' --output text)"
-        aws ecr put-image --repository-name "$REPOSITORY" --image-tag "$1" --image-manifest "$MANIFEST"
-      else
-        echo "Failed to tag $REPOSITORY:$1"
-        exit 1
-      fi
-      ;;
-    *)
-      echo "Build failed or was stopped. Exiting."
+ok="false"
+echo "Waiting for build status for $2..."
+for i in $(seq 1 $MAX_TRIES); do
+  return_value=$(eval "${TEST_COMMAND}")
+  if [ "$return_value" == "SUCCEEDED" ]; then
+    ok="true"
+    echo "Found successful build for $2. Tagging."
+    if [[ "$(aws ecr describe-images --repository-name "$REPOSITORY" --image-ids imageTag="$2"|jq -r .imageDetails[].imageTags[])" == "$2" ]]; then
+      MANIFEST="$(aws ecr batch-get-image --repository-name "$REPOSITORY" --image-ids imageTag="$2" --query 'images[].imageManifest' --output text)"
+      aws ecr put-image --repository-name "$REPOSITORY" --image-tag "$1" --image-manifest "$MANIFEST"
+      break
+    else
+      echo "Failed to tag $REPOSITORY:$1"
       exit 1
-      ;;
-  esac
-done < <(aws codebuild batch-get-builds --ids $JOBS | jq -r --arg COMMIT_ID "$2" '.builds[] | select(.resolvedSourceVersion | contains($COMMIT_ID)) | .buildStatus')
+    fi
+  else
+    echo -n "."
+  fi
+  sleep 5
+done
+if [ "$ok" == "false" ]; then
+  echo "error"
+  exit 1
+fi
