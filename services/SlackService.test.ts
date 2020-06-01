445f3a7827a5419b8cce38effc11d7daa239916e
Ryan Travitz <ryan.travitz@adhocteam.us>
2020-06-01 15:34:24 -0700

    Change SlackService to use webhook instead of app (#56)

--- services/SlackService.test.ts
+++ services/SlackService.test.ts
@@ -2,14 +2,18 @@ import axios, { AxiosInstance } from 'axios';
 import SlackService from './SlackService';
 
 describe('SlackService', () => {
+  const hookUrl = 'https://beacons.gondor.gov';
+  const hookConfig = {
+    channel: '#gondor-chat',
+    username: 'StewardBot',
+    icon_emoji: ':minas-tirith:'
+  };
+
   it('sends a provided bearer token', () => {
     const mockCreate = jest.spyOn(axios, 'create');
-    new SlackService('channel123', 'fakeToken');
+    new SlackService(hookUrl, hookConfig);
 
-    expect(mockCreate).toHaveBeenCalledWith({
-      baseURL: 'https://slack.com/api',
-      headers: { 'Authorization': `Bearer fakeToken` }
-    });
+    expect(mockCreate).toHaveBeenCalledWith({ baseURL: hookUrl });
   });
 
   it('sends a success message', async () => {
@@ -17,22 +21,22 @@ describe('SlackService', () => {
       status: 200,
       statusText: 'ok',
       headers: {},
-      data: {
-        channel: 'channel123'
-      },
+      data: 'ok',
     });
 
     // cast to unknown first to avoid having to reimplement all of AxiosInstance
     jest.spyOn(axios, 'create').mockImplementation(() => ({ post: mockPost } as unknown as AxiosInstance));
 
-    const message = "Test, User: test@example.com\nRequested access to:\n* va_facilities\n* health\n";
-    const service = new SlackService('channel123', 'faketoken');
+    const message = "Son of Denethor, Faramir: faramir@rangers.gondor.mil\nRequested access to:\n* va_facilities\n* health\n";
+    const service = new SlackService(hookUrl, hookConfig);
 
     const res = await service.sendSuccessMessage(message, 'New User Application');
 
-    expect(res.channel).toEqual('channel123');
-    expect(mockPost).toHaveBeenCalledWith('/chat.postMessage', {
-      channel: 'channel123',
+    expect(res).toEqual('ok');
+    expect(mockPost).toHaveBeenCalledWith('', {
+      channel: hookConfig.channel,
+      username: hookConfig.username,
+      icon_emoji: hookConfig.icon_emoji,
       text: '',
       attachments: [{
         text: message,
