d88e87519aef53a7793fcc1a5623b140740eaeba
Michael Kimball <michael.kimball@oddball.io>
2020-08-20 18:47:03 -0400

    API-1667 Update .test.env -> .env.test

--- dotenv-config.js
+++ dotenv-config.js
@@ -1,3 +1,3 @@
-// dotenv doesn't provide an override option, so this will allow the .test.env to decide the environment
+// dotenv doesn't provide an override option, so this will allow the .env.test to decide the environment
 delete process.env.NODE_ENV;
-require('dotenv').config({ path: './.test.env' });
+require('dotenv').config({ path: './.env.test' });
