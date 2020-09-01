ffdc587d9e719be1828afe125b7a914ef754f99d
nathanbwright <nathan.wright@oddball.io>
2020-09-01 15:17:49 -0600

    Checkpoint. Adding authz endpoints to okta.

--- services/OktaService.ts
+++ services/OktaService.ts
@@ -10,6 +10,12 @@ export interface OktaApplicationResponse {
     };
   };
 }
+const authzEndpoints = {
+  'health': 'aus7y0ho1w0bSNLDV2p7',
+  'verification': 'aus7y0sefudDrg2HI2p7',
+  // need to add communityCare
+  'claims': 'aus7y0lyttrObgW622p7',
+};
 export default class OktaService implements MonitoredService {
   public client: Client;
 
@@ -24,6 +30,18 @@ export default class OktaService implements MonitoredService {
   public async createApplication(app: OktaApplication, groupID: string): Promise<OktaApplicationResponse> {
     const resp = await this.client.createApplication(app.toOktaApp());
     await this.client.createApplicationGroupAssignment(resp.id, groupID);
+
+    const applicableEndpoints = app.owner.apiList
+      .filter(endpoint => authzEndpoints[endpoint])
+      .map(endpoint => authzEndpoints[endpoint]);
+
+    await Promise.all(applicableEndpoints.map(async (authServerId) => {
+      const policy = await this.client.listAuthorizationServerPolicies(authServerId);
+      const clientId = resp.credentials.oauthClient.client_id;
+      // need to add clientID to the policy.conditions.clients.include
+      await this.client.updateAuthorizationServerPolicy(authServerId, policy.id, policy);
+    }));
+
     return resp;
   }
 
