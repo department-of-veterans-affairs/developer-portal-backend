3b513a0010b5f94a0fd4e9de0967b5ee9d2ba379
nathanbwright <nathan.wright@oddball.io>
2020-09-14 11:26:19 -0600

    Don't use quotes for OKTA_AUTHZ_ENDPOINTS keys.

--- config/apis.ts
+++ config/apis.ts
@@ -6,10 +6,10 @@ export const KONG_CONSUMER_APIS: string[] = [
 ];
 
 export const OKTA_AUTHZ_ENDPOINTS = {
-  'health':        'aus7y0ho1w0bSNLDV2p7',
-  'verification':  'aus7y0sefudDrg2HI2p7',
-  'communityCare': 'aus89xnh1xznM13SK2p7',
-  'claims':        'aus7y0lyttrObgW622p7',
+  health:        'aus7y0ho1w0bSNLDV2p7',
+  verification:  'aus7y0sefudDrg2HI2p7',
+  communityCare: 'aus89xnh1xznM13SK2p7',
+  claims:        'aus7y0lyttrObgW622p7',
 };
 
 export const OKTA_CONSUMER_APIS: string[] = Object.keys(OKTA_AUTHZ_ENDPOINTS);
