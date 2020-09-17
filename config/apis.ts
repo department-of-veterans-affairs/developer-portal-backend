e03460e84e05fcdd1c83d547fbbf8a214d241529
nathanbwright <nathan.wright@oddball.io>
2020-09-17 10:19:09 -0600

    Update authz env variable names.

--- config/apis.ts
+++ config/apis.ts
@@ -6,10 +6,10 @@ export const KONG_CONSUMER_APIS: string[] = [
 ];
 
 export const OKTA_AUTHZ_ENDPOINTS = {
-  health:        process.env.OKTA_AUTHZ_HEALTH,
-  verification:  process.env.OKTA_AUTHZ_VERIFICATION,
-  communityCare: process.env.OKTA_AUTHZ_COMMUNITYCARE,
-  claims:        process.env.OKTA_AUTHZ_CLAIMS,
+  health:        process.env.AUTHZ_SERVER_HEALTH,
+  verification:  process.env.AUTHZ_SERVER_VERIFICATION,
+  communityCare: process.env.AUTHZ_SERVER_COMMUNITYCARE,
+  claims:        process.env.AUTHZ_SERVER_CLAIMS,
 };
 
 export const OKTA_CONSUMER_APIS: string[] = Object.keys(OKTA_AUTHZ_ENDPOINTS);
