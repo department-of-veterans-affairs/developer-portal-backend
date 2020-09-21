b6b45aee01c0db2febeb6838d16a452bdfe7fb25
Anthony Kopczyk <anthony.kopczyk@oddball.io>
2020-09-21 09:12:55 -0400

    Add missing test for new schemaValidators
    I moved the validateApiList function into its own file but didn't give
    it its own tests. Now it has them.

---
+++ routes/schemaValidators/validateApiList.test.ts
@@ -0,0 +1,27 @@
+import 'jest';
+import validateApiList from './validateApiList';
+
+describe('validateApiList', () => {
+  it('allows supported api values', () => {
+    const apis = 'benefits';
+    const result = validateApiList(apis);
+    expect(result).toEqual(apis);
+  });
+
+  it('supports comma separated api values', () => {
+    const apis = 'benefits,communityCare';
+    const result = validateApiList(apis);
+    expect(result).toEqual(apis);
+  });
+
+  it('only allows supported api values', () => {
+    const apis = 'benefits,horsies';
+    expect(() => validateApiList(apis)).toThrow('invalid apis in list');
+  });
+
+  it('throws error when unable to process the list', () => {
+    const apis = 123 as unknown as string;
+    expect(() => validateApiList(apis))
+      .toThrow('it was unable to process the provided data');
+  });
+})
