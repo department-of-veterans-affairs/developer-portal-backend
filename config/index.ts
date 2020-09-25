2b4f91324af0ae8bea8eec023b49d280d17aae92
nathanbwright <nathan.wright@oddball.io>
2020-09-25 13:47:44 -0600

    Refactor to generate the apisToAcls and APIS_TO_PROPER_NAMES constants. Change type structure.

--- config/index.ts
+++ config/index.ts
@@ -1,103 +1,76 @@
 type OAuthEndpoint = {
-  oauthEndpoint: true;
   properName: string;
   internalName: string;
   authzEndpoint: string | undefined;
 };
 type KeyBasedEndpoint = {
-  oauthEndpoint: false;
   properName: string;
   internalName: string;
   acl: string;
 };
 
-type ApiEndpoint = OAuthEndpoint | KeyBasedEndpoint;
-
-const apiEndpointList: ApiEndpoint[] = [
+const oauthEndpointList: OAuthEndpoint[] = [
   {
-    oauthEndpoint: true,
     properName: "Health API",
     internalName: "health",
     authzEndpoint: process.env.AUTHZ_SERVER_HEALTH,
   },
   {
-    oauthEndpoint: true,
     properName: "Veteran Verification API",
     internalName: "verification",
     authzEndpoint: process.env.AUTHZ_SERVER_VERIFICATION,
   },
   {
-    oauthEndpoint: true,
     properName: "Community Care Eligibility API",
     internalName: "communityCare",
     authzEndpoint: process.env.AUTHZ_SERVER_COMMUNITYCARE,
   },
   {
-    oauthEndpoint: true,
     properName: "Claims API",
     internalName: "claims",
     authzEndpoint: process.env.AUTHZ_SERVER_CLAIMS,
   },
+];
 
+const keyBasedEndpointList: KeyBasedEndpoint[] = [
   {
-    oauthEndpoint: false,
     properName: "Benefits Intake API",
     internalName: "benefits",
     acl: "vba_documents",
   },
   {
-    oauthEndpoint: false,
     properName: "VA Facilities API",
     internalName: "facilities",
     acl: "va_facilities",
   },
   {
-    oauthEndpoint: false,
     properName: "VA Form API",
     internalName: "vaForms",
     acl: "va_forms",
   },
   {
-    oauthEndpoint: false,
     properName: "Veteran Confirmation API",
     internalName: "confirmation",
     acl: "veteran_confirmation",
   },
 ];
 
-export const apisToAcls = {
-  appeals: "appeals",
+export const apisToAcls = keyBasedEndpointList.reduce((obj, item) => {
+  return {
+    ...obj,
+    [item.internalName]: item.acl,
+  };
+}, {});
 
-  // all the ones below here could be generated...
-  benefits: "vba_documents",
-  confirmation: "veteran_confirmation",
-  facilities: "va_facilities",
-  vaForms: "va_forms",
-  verification: "veteran_verification",
-};
+export const APIS_TO_PROPER_NAMES = {};
+for (const endpoint of [...oauthEndpointList, ...keyBasedEndpointList]) {
+  APIS_TO_PROPER_NAMES[endpoint.internalName] = endpoint.properName
+}
 
-export const APIS_TO_PROPER_NAMES = {
-  appeals: "Appeals Status API",
-
-  // all the ones below here could be generated...
-  benefits: "Benefits Intake API",
-  claims: "Claims API",
-  communityCare: "Community Care Eligibility API",
-  confirmation: "Veteran Confirmation API",
-  facilities: "VA Facilities API",
-  health: "Health API",
-  vaForms: "VA Form API",
-  verification: "Veteran Verification API",
-};
-
-export const API_LIST: string[] = apiEndpointList.map((x) => x.internalName);
-
-export const KONG_CONSUMER_APIS: string[] = apiEndpointList
-  .filter((x) => x.oauthEndpoint === false)
+export const KONG_CONSUMER_APIS: string[] = keyBasedEndpointList
   .map((x) => x.internalName);
 
-export const OKTA_AUTHZ_ENDPOINTS = apiEndpointList
-  .filter((x) => x.oauthEndpoint === true)
+export const OKTA_AUTHZ_ENDPOINTS = oauthEndpointList
   .reduce((obj, item) => {
     return {
       ...obj,
@@ -106,3 +79,5 @@ export const OKTA_AUTHZ_ENDPOINTS = apiEndpointList
   }, {});
 
 export const OKTA_CONSUMER_APIS: string[] = Object.keys(OKTA_AUTHZ_ENDPOINTS);
+
+export const API_LIST: string[] = KONG_CONSUMER_APIS.concat(OKTA_CONSUMER_APIS);
