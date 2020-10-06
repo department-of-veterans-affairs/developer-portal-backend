5b0f5396ec17af0da28893e4e3dd9544182b7261
nathanbwright <nathan.wright@oddball.io>
2020-10-06 14:24:35 -0600

    Simplify some of the test setup.

--- routes/DeveloperApplication.integration.ts
+++ routes/DeveloperApplication.integration.ts
@@ -122,15 +122,17 @@ describe('/developer_application', () => {
     });
 
     describe('pre-existing kong', () => {
-      it('consumer', async () => {
-        const path = '/internal/admin/consumers/FellowshipBaggins';
-        const interceptor = kong.get(path);
-        nock.removeInterceptor(interceptor);
+      const consumerPath = '/internal/admin/consumers/FellowshipBaggins';
+      const consumerInterceptor = kong.get(consumerPath);
 
-        kong.get(path).reply(200, {
+      beforeEach(() => {
+        nock.removeInterceptor(consumerInterceptor);
+        kong.get(consumerPath).reply(200, {
           id: '123', created_at: 1008720000, username: 'frodo', custom_id: '222', tags: null,
         });
+      });
 
+      it('consumer', async () => {
         const response = await request.post('/developer_application').send(devAppRequest);
 
         expect(response.status).toEqual(200);
@@ -142,22 +144,15 @@ describe('/developer_application', () => {
       });
 
       it('consumer and one pre-existing acl', async () => {
-        const consumerPath = '/internal/admin/consumers/FellowshipBaggins';
-        const interceptor = kong.get(consumerPath);
-        nock.removeInterceptor(interceptor);
-
         const aclPath = `${consumerPath}/acls`;
         const aclInterceptor = kong.get(aclPath);
         nock.removeInterceptor(aclInterceptor);
 
-        kong.get(consumerPath).reply(200, {
-          id: '123', created_at: 1008720000, username: 'frodo', custom_id: '222', tags: null,
-        })
-          .get(aclPath).reply(200, {
-            total: 2, data: [
-              { group: 'va_facilities', created_at: 1040169600, id: '123', consumer: { id: '222' } },
-            ],
-          });
+        kong.get(aclPath).reply(200, {
+          total: 2, data: [
+            { group: 'va_facilities', created_at: 1040169600, id: '123', consumer: { id: '222' } },
+          ],
+        });
 
         const response = await request.post('/developer_application').send(devAppRequest);
 
@@ -170,23 +165,17 @@ describe('/developer_application', () => {
       });
 
       it('consumer and two pre-existing acls', async () => {
-        const consumerPath = '/internal/admin/consumers/FellowshipBaggins';
-        const interceptor = kong.get(consumerPath);
-        nock.removeInterceptor(interceptor);
-
         const aclPath = `${consumerPath}/acls`;
         const aclInterceptor = kong.get(aclPath);
         nock.removeInterceptor(aclInterceptor);
 
-        kong.get(consumerPath).reply(200, {
-          id: '123', created_at: 1008720000, username: 'frodo', custom_id: '222', tags: null,
-        })
-          .get(aclPath).reply(200, {
-            total: 2, data: [
-              { group: 'va_facilities', created_at: 1040169600, id: '123', consumer: { id: '222' } },
-              { group: 'veteran_verification', created_at: 1040169600, id: '123', consumer: { id: '222' } },
-            ],
-          });
+        kong.get(aclPath).reply(200, {
+
+          total: 2, data: [
+            { group: 'va_facilities', created_at: 1040169600, id: '123', consumer: { id: '222' } },
+            { group: 'veteran_verification', created_at: 1040169600, id: '123', consumer: { id: '222' } },
+          ],
+        });
 
         const response = await request.post('/developer_application').send(devAppRequest);
 
