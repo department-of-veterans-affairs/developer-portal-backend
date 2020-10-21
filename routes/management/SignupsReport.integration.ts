69305e4bcf8b503afc0d76ab28b4da7f2b8328c7
nathanbwright <nathan.wright@oddball.io>
2020-10-21 15:25:25 -0600

    Add tests for when slack and dynamo fail.

--- routes/management/SignupsReport.integration.ts
+++ routes/management/SignupsReport.integration.ts
@@ -6,13 +6,14 @@ import configureApp from '../../app';
 
 const request = supertest(configureApp());
 describe('/reports/signups', () => {
+  const dynamoDB = nock(`${process.env.DYNAMODB_ENDPOINT}`);
+  const slack = nock(process.env.SLACK_BASE_URL);
+
   it('sends a message to slack', async () => {
     // This test is just basically just a sanity check in it's current implementation...
     // - it does not verify that the payload is correct, it just verifies that slack recieved a 'post' to the correct path
-    const dynamoDB = nock(`${process.env.DYNAMODB_ENDPOINT}`);
     dynamoDB.post('/').reply(200).post('/').reply(200);
 
-    const slack = nock(process.env.SLACK_BASE_URL);
     slack.post('/api/chat.postMessage').reply(200);
 
     expect(slack.isDone()).toEqual(false);
@@ -20,6 +21,28 @@ describe('/reports/signups', () => {
     expect(slack.isDone()).toEqual(true);
 
     expect(response.status).toEqual(200);
-    expect(response.body).toEqual({});
+    expect(response.text).toEqual('OK');
+  });
+
+  it('sends a 500 response if slack responds with 500', async () => {
+    dynamoDB.post('/').reply(200).post('/').reply(200);
+
+    slack.post('/api/chat.postMessage').reply(500);
+
+    const response = await request.get('/reports/signups').send({});
+
+    expect(response.body.message).toContain('500');
+    expect(response.status).toEqual(500);
+  });
+
+  it('sends a 500 response if dynamoDB responds with 500', async () => {
+    dynamoDB.post('/').reply(500).persist();
+
+    slack.post('/api/chat.postMessage').reply(200);
+
+    const response = await request.get('/reports/signups').send({});
+
+    expect(response.body.message).toContain('500');
+    expect(response.status).toEqual(500);
   });
 });
