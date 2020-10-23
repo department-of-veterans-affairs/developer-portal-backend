9bb67ad19c9ac5ca4491fca417e9a04ba1b9fd28
nwalker-va <nicole.houston@oddball.io>
2020-10-23 15:08:04 -0500

    fix message in test

--- services/SlackService.integration.ts
+++ services/SlackService.integration.ts
@@ -1,13 +1,10 @@
 import 'jest';
-import supertest from 'supertest';
-import configureApp from '../app';
 import SlackService from './SlackService';
 require('dotenv').config({ path: './.env' });
 
-const request = supertest(configureApp());
 describe('Slack API', () => {
     it('should not return warning: "missing_charset" in the response data', async () => {
-        const message = "A";
+        const message = "Even the smallest person can change the course of the future.";
         const slack = new SlackService(process.env.SLACK_BASE_URL, process.env.SLACK_TOKEN, {channel: process.env.SLACK_CHANNEL, bot: process.env.SLACK_BOT});
 
         const response = await slack.sendSuccessMessage(message, 'New User Signup');
