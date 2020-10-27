037d7211e1693bbe8925a03dd1d7bb0ca44813dd
Keifer Furzland <kfrz@users.noreply.github.com>
2020-10-27 13:02:17 -0500

    Update Sentry.ts

--- config/Sentry.ts
+++ config/Sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/node';
 const { SENTRY_DSN, SENTRY_ENV } = process.env;
 Sentry.init({
   dsn: SENTRY_DSN,
-  environment: SENTRY_ENV
+  environment: SENTRY_ENV,
 });
 
 export default Sentry;
