feb2972a6155cbd07d37efa1aacf2684ce010ebc
Braden Shipley <bradenrshipley@gmail.com>
2021-06-24 13:16:06 -0500

    removed dead code from ContactUs.ts
    Signed-off-by: Braden Shipley <bradenrshipley@gmail.com>

--- routes/ContactUs.ts
+++ routes/ContactUs.ts
@@ -32,58 +32,7 @@ export type PublishingSupportRequest = {
   apiOtherInfo?: string;
 } & ContactDetails
 
-export type MonitizationInformation = {
-  monitizedVeteranInformation: boolean;
-  monitizationExplanation?: string;
-  veteranFacing?: boolean;
-  website?: string;
-  signUpLink?: string;
-  supportLink?: string;
-  platforms?: string[];
-  veteranFacingDescription?: string;
-}
-
-export type TechnicalInformation = {
-  vasiSystemName?: string;
-  credentialStorage: string;
-  storePIIOrPHI: boolean;
-  storageMethod?: string;
-  safeguards?: string;
-  breachManagementProcess?: string;
-  vulnerabilityManagement?: string;
-  exposeHealthInformationToThirdParties?: boolean;
-  thirdPartyHealthInfoDescription?: string;
-  scopesAccessRequested?: string[];
-  distrubitingAPIKeysToCustomers?: boolean;
-  namingConvention?: string;
-  centralizedBackendLog: string;
-}
-
-export type ProductionContact = {
-  firstName: string;
-  lastName: string;
-  email:string;
-}
-export type ProductionAccessRequest = {
-  type: SubmissionType.PRODUCTION_ACCESS;
-  primaryContact: ProductionContact;
-  secondaryContact: ProductionContact;
-  email: string;
-  organization: string;
-  appName: string;
-  appDescription: string;
-  statusUpdateEmails: string[];
-  valueProvided: string;
-  businessModel?: string;
-  monitization: MonitizationInformation;
-  //TODO: ask question about screenshots in form. Can they be ignored? Should I handle them as email attachments?
-  technicalInformation: TechnicalInformation;
-  policyDocuments: string[];
-  phoneNumber: string;
-  apis?: string[];
-}
-
-type SupportRequest = ConsumerSupportRequest | PublishingSupportRequest | ProductionAccessRequest
+type SupportRequest = ConsumerSupportRequest | PublishingSupportRequest
 
 export const contactSchema = Joi.object().keys({
   firstName: Joi.string().required(),
@@ -126,26 +75,7 @@ export default function contactUsHandler(govDelivery: GovDeliveryService) {
 
         await govDelivery.sendPublishingSupportEmail(supportRequest);
         res.sendStatus(200);
-      } else if (req.body.type === SubmissionType.PRODUCTION_ACCESS) {
-        //TODO: Determine if this needs specific fields that differ from PUBLISHING
-        const supportRequest: ProductionAccessSupportEmail = {
-          primaryContact: req.body.primaryContact,
-          secondaryContact: req.body.secondaryContact,
-          requester: req.body.email,
-          organization: req.body.organization,
-          appName: req.body.appName,
-          appDescription: req.body.appDescription,
-          statusUpdateEmails: req.body.statusUpdateEmails,
-          valueProvided: req.body.valueProvided,
-          monitization: req.body.monitization,
-          technicalInformation: req.body.technicalInformation,
-          policyDocuments: req.body.policyDocuments,
-          phoneNumber: req.body.phoneNumber,
-        };
-
-        await govDelivery.sendProductionAccessEmail(supportRequest);
-        res.sendStatus(200);
-      }else {
+      } else {
         const supportRequest: ConsumerSupportEmail = {
           firstName: req.body.firstName,
           lastName: req.body.lastName,
