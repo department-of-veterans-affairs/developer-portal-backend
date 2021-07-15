2caad464436457933c5c2f11fd4d5ee66ecca4b9
Alexander Valencia <alex.valencia@adhocteam.us>
2021-07-15 13:54:52 -0500

    lint: update eslint/prettier to better match dev-portal

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,15 @@ const rules = {
   '@typescript-eslint/camelcase': 'off',
   'comma-dangle': ["error", "always-multiline"],
   indent: ['error', 2],
+  '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
 };
 
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 2020,
     tsconfigRootDir: __dirname,
     project: [
       './tsconfig.json',
@@ -18,17 +21,27 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'prettier'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   env: {
     'node': true,
   },
   rules: {
     ...rules,
+  },
+  settings: {
+    node: {
+      allowModules: ['jest', 'nock', 'supertest'],
+      tryExtensions: ['.js', '.json', '.node', '.ts', '.d.ts'],
+    },
   },
   overrides: [
     {
